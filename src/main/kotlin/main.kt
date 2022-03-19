2b90a41d7b3f47fcc3fdfe26509244781a0cf337
Anita Kosman <anitakosman@hotmail.com>
2022-03-19 11:55:30 +0100

    Add subscriptions for phedny

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.dataformat.xml.XmlMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import java.io.File
 import java.net.URL
-import java.time.ZonedDateTime
 import java.util.*
 
 fun main() {
@@ -23,10 +22,15 @@ fun main() {
 
 val logFile = File("logs/${System.currentTimeMillis()}.txt")
 
+const val base = "http://127.0.0.1:32400"
+const val tokenQuery = "?X-Plex-Token="
+const val tokenAnita = "xxyBuhnssASHQcCwh-hx"
+const val tokenPhedny = "opVisCZwVzuZaK3_nXt5"
+val libraryURL = URL("$base/library/sections/3/all$tokenQuery$tokenAnita")
+
+val subscriptions = mapOf("3918" to listOf(tokenAnita, tokenPhedny)).withDefault { listOf(tokenAnita) }
+
 private fun checkAndDelete() {
-    val base = "http://127.0.0.1:32400"
-    val token = "?X-Plex-Token=xxyBuhnssASHQcCwh-hx"
-    val libraryURL = URL("$base/library/sections/3/all$token")
     val xmlMapper = XmlMapper(JacksonXmlModule().apply {
         setDefaultUseWrapper(false)
     }).registerKotlinModule()
@@ -34,15 +38,35 @@ private fun checkAndDelete() {
         .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 
     val watchedSeries = xmlMapper.readValue(libraryURL, MediaContainerLibrary::class.java).directory
-        .filter { it.viewedLeafCount > 0 }
-    val videos = watchedSeries.map {
-        xmlMapper.readValue(
-            URL(base + it.key.replace("children", "allLeaves") + token),
-            MediaContainerSeries::class.java
-        )
-    }
-        .flatMap { series -> series.video.filter { it.viewCount != null }.map { video -> video to series.video.none { it.viewCount == null } }  }
-    val files = videos.map { xmlMapper.readValue(URL(base + it.first.key + token), MediaContainerSeries::class.java) to it.second }
+        .filter { it.viewedLeafCount > 0 || subscriptions.getValue(it.ratingKey).contains(tokenPhedny) }
+
+    val videos = watchedSeries
+        .map { directory ->
+            val countedVideos = subscriptions.getValue(directory.ratingKey)
+                .map { token ->
+                    xmlMapper.readValue(
+                        URL(base + directory.key.replace("children", "allLeaves") + tokenQuery + token),
+                        MediaContainerSeries::class.java
+                    )
+                }
+                .flatMap { it.video }
+                .groupBy { it.key }
+                .map { (key, videos) -> CountedVideo(key, videos.all { it.viewCount != null }) }
+            CountedMediaContainerSeries(countedVideos, countedVideos.all { it.allWatched })
+        }
+        .flatMap { series ->
+            series.videos
+                .filter { it.allWatched }
+                .map { RemovableVideo(it.key, series.seriesDone) }
+        }
+
+    val files = videos
+        .map {
+            xmlMapper.readValue(
+                URL(base + it.key + tokenQuery + tokenAnita),
+                MediaContainerSeries::class.java
+            ) to it.seriesDone
+        }
         .flatMap { (mc, seriesDone) ->
             mc.video.flatMap { video ->
                 video.media.flatMap { media ->
@@ -65,16 +89,22 @@ fun findUpperSeriesFile(file: File, seriesDone: Boolean): File {
         f = p
         p = p.parentFile
     }
-    return if(seriesDone || f.name.matches(Regex(".*s\\d\\de\\d\\d.*", RegexOption.IGNORE_CASE))) f else a!!
+    return if (seriesDone || f.name.matches(Regex(".*s\\d\\de\\d\\d.*", RegexOption.IGNORE_CASE))) f else a!!
 }
 
 data class MediaContainerLibrary(val directory: List<Directory>)
 
-data class Directory(val key: String, val viewedLeafCount: Int)
+data class Directory(val key: String, val viewedLeafCount: Int, val ratingKey: String)
 
 data class MediaContainerSeries(val video: List<Video>)
 
+data class CountedMediaContainerSeries(val videos: List<CountedVideo>, val seriesDone: Boolean)
+
 data class Video(val viewCount: Int?, val key: String, val media: List<Media>)
+
+data class CountedVideo(val key: String, val allWatched: Boolean)
+
+data class RemovableVideo(val key: String, val seriesDone: Boolean)
 
 data class Media(val part: List<Part>)
 
